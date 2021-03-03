9ae184fa285e2f1042cefb6503c1d4e5b1d5b91e
ACR1209 <78624635+ACR1209@users.noreply.github.com>
2021-03-02 20:33:48 -0500

    Update OrtogonalProyection.java

--- OrtogonalProyection/src/ortogonalproyection/OrtogonalProyection.java
+++ OrtogonalProyection/src/ortogonalproyection/OrtogonalProyection.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ortogonalproyection;
 
 import gui.MainFrm;
 
 /**
- *
- * @author USUARIO
+ * Programa para realizar la proyección ortogonal por el método de Gram-Schmidt
+ * @author Andrés Coronel
  */
 public class OrtogonalProyection {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {            
         MainFrm.main(args);
     }
